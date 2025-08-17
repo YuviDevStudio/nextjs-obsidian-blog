72789f9e8bf9d1da849d6c6e2a55ecdc81149879
YuviDevStudio <yuvidevstudio@gmail.com>
2025-08-17 02:27:46 -0600

    cloudflare errors 5

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,20 +1,26 @@
 import { getPostData } from '../../../lib/posts';
 import Date from '../../components/date';
 import MarkdownRenderer from '../../components/markdown-renderer';
+import { notFound } from 'next/navigation';
 
 export const runtime = 'edge';
 
 export async function generateMetadata({ params }) {
-  const resolvedParams = await params;
-  const postData = await getPostData(resolvedParams.id);
+  const postData = await getPostData(params.id);
+  if (!postData) {
+    return notFound();
+  }
   return {
     title: postData.title,
   };
 }
 
 export default async function Post({ params }) {
-  const resolvedParams = await params;
-  const postData = await getPostData(resolvedParams.id);
+  const postData = await getPostData(params.id);
+  if (!postData) {
+    return notFound();
+  }
+
   return (
     <>
       <article className='px-4'>
